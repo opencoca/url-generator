ebc6e52dc192a8a46236d70efdd572a5f29d9fff
Vinicius Reis <luiz.vinicius73@gmail.com>
2019-09-06 16:47:08 -0300

    fa72592

---
+++ precache-manifest.4829c28664017708ecba141cdd36f2be.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "d864378996c8359239b6",
+    "url": "/gravatar-url-generator/css/page-adorable~page-dicebear~page-gravatar~page-robohash.82533376.css"
+  },
+  {
+    "revision": "d6ae55aaef1fcc0878c7d0937da615e7",
+    "url": "/gravatar-url-generator/index.html"
+  },
+  {
+    "revision": "bb861e653ddcadcb2896",
+    "url": "/gravatar-url-generator/js/chunk-vendors-legacy.09724904.js"
+  },
+  {
+    "revision": "f57fa1d8480561335b62",
+    "url": "/gravatar-url-generator/js/page-robohash-legacy.2b7536f9.js"
+  },
+  {
+    "revision": "4a05a1eb70fad92e61a1",
+    "url": "/gravatar-url-generator/js/page-about-legacy.c32f223b.js"
+  },
+  {
+    "revision": "b37829010f71260d388a",
+    "url": "/gravatar-url-generator/js/page-adorable-legacy.49faeead.js"
+  },
+  {
+    "revision": "a1283048e4d7958452b2",
+    "url": "/gravatar-url-generator/js/app-legacy.b88e498d.js"
+  },
+  {
+    "revision": "d864378996c8359239b6",
+    "url": "/gravatar-url-generator/js/page-adorable~page-dicebear~page-gravatar~page-robohash-legacy.e8ffa33d.js"
+  },
+  {
+    "revision": "856b85f3cf73aaedd426",
+    "url": "/gravatar-url-generator/js/page-dicebear-legacy.7bc48ea3.js"
+  },
+  {
+    "revision": "e8eb22916dede448e942",
+    "url": "/gravatar-url-generator/js/page-gravatar-legacy.eaef0715.js"
+  },
+  {
+    "revision": "4a05a1eb70fad92e61a1",
+    "url": "/gravatar-url-generator/css/page-about.638fff7c.css"
+  },
+  {
+    "revision": "a1283048e4d7958452b2",
+    "url": "/gravatar-url-generator/css/app.d580c61b.css"
+  }
+];
---
+++ precache-manifest.8a384921ecad60fe8b7d356e65cc58c5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "acb349c449cca492bb5a",
+    "url": "/gravatar-url-generator/css/page-adorable~page-dicebear~page-gravatar~page-robohash.82533376.css"
+  },
+  {
+    "revision": "0beb767ab7debed9a5c3895af04ffb16",
+    "url": "/gravatar-url-generator/precache-sw.js"
+  },
+  {
+    "revision": "acb349c449cca492bb5a",
+    "url": "/gravatar-url-generator/js/page-adorable~page-dicebear~page-gravatar~page-robohash.ce865754.js"
+  },
+  {
+    "revision": "ca1a66445907c9fb57cd",
+    "url": "/gravatar-url-generator/js/chunk-vendors.66fbb250.js"
+  },
+  {
+    "revision": "c5872306d3f15920fe76",
+    "url": "/gravatar-url-generator/js/page-about.ed716deb.js"
+  },
+  {
+    "revision": "6ff1795bcdf6d2a273fb",
+    "url": "/gravatar-url-generator/js/page-adorable.a0fa626e.js"
+  },
+  {
+    "revision": "d2a9082f0d111991b079",
+    "url": "/gravatar-url-generator/js/page-robohash.9f2aa3e9.js"
+  },
+  {
+    "revision": "b81349049d08a31c6128",
+    "url": "/gravatar-url-generator/js/page-gravatar.c4f43a6a.js"
+  },
+  {
+    "revision": "db212e001a541141ef0e",
+    "url": "/gravatar-url-generator/js/page-dicebear.6a84ad3d.js"
+  },
+  {
+    "revision": "67b2d8df37f8fbbe5831",
+    "url": "/gravatar-url-generator/js/app.36ff2119.js"
+  },
+  {
+    "revision": "0e4f90576c681e8f2ac751fd68ba8e37",
+    "url": "/gravatar-url-generator/index.html"
+  },
+  {
+    "revision": "c5872306d3f15920fe76",
+    "url": "/gravatar-url-generator/css/page-about.638fff7c.css"
+  },
+  {
+    "revision": "67b2d8df37f8fbbe5831",
+    "url": "/gravatar-url-generator/css/app.d580c61b.css"
+  }
+];
